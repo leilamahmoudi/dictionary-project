ce538f62f1dc8523794b7bf443e30d5dcd2ea884
leilamahmoudi <leila.mahmoudi9169@gmail.com>
2021-06-08 11:13:39 +0200

    Added dictionary api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
+
+  function handelResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Searchin for ${keyword} `);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handelResponse);
   }
   function handelKeywordChange(event) {
     setKeyword(event.target.value);
