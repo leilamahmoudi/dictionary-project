08d084a09c01f046277ca78fb43c99cee32e7026
leilamahmoudi <leila.mahmoudi9169@gmail.com>
2021-06-08 11:17:07 +0200

    Added api documentation

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -12,6 +12,9 @@ export default function Dictionary() {
   function search(event) {
     event.preventDefault();
     alert(`Searchin for ${keyword} `);
+
+    //documentation : https://dictionaryapi.dev/
+
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handelResponse);
   }
